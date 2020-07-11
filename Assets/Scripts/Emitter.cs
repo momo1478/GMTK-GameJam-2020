fedc596ba9647a78c6809a8fe493380f9fd387af
Harrison Gottlieb <hlgott1593@gmail.com>
2020-07-10 21:05:43 -0700

    adding bullet, emitter, projectile

---
+++ Assets/Scripts/Emitter.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Emitter : MonoBehaviour
+{
+
+    public float rate = 10;
+    public float speed = 5;
+
+    public bool randomAngle = true;
+    public float minAngle = 0f;
+    public float maxAngle = 360f;
+
+    public Pool pool;
+
+    public Projectile projectile;
+
+    private float tillNextAction = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    public Vector3 GetVelocity(float speed, float degree)
+    {
+        float radians = degree * (Mathf.PI / 180);
+        return new Vector3(Mathf.Cos(radians), Mathf.Sin(radians), 0) * speed;
+    }
+
+    public void SpawnBullet()
+    {
+        Projectile clone = Instantiate(projectile, transform.position, transform.rotation);
+        float angle = Random.Range(minAngle, maxAngle);
+        clone.data = new Projectile.ProjectileData(GetVelocity(speed, angle));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (tillNextAction > 1.0f/rate)
+        {
+            SpawnBullet();
+            tillNextAction = 0f;
+        }
+        else
+        {
+            tillNextAction += Time.deltaTime;
+        }
+    }
+}
