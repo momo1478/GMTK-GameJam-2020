42acbd89ef84ec3af23a6818c7cddbaff229c440
kgc00Zenbook <kirbygc00@gmail.com>
2020-07-11 10:19:05 -0700

    finished safe zone

---
+++ Assets/Scripts/Safe Zones/SafeZoneActiveState.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Animations;
+
+namespace Safe_Zones {
+    public class SafeZoneActiveState : StateMachineBehaviour {
+        private float stateTimeLeft;
+        [SerializeField] private float activeDuration;
+        private static readonly int Closing = Animator.StringToHash("Closing");
+        [SerializeField] private float range;
+        private CircleCollider2D collider;
+        private float lerpTimeLeft;
+        [SerializeField] private float lerpDuration = .75f;
+        private ParticleSystem particleSystem;
+        [SerializeField] private GameObject particleSystemObject;
+
+        public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex,
+            AnimatorControllerPlayable controller) {
+            stateTimeLeft = activeDuration;
+            lerpTimeLeft = lerpDuration;
+            collider = animator.gameObject.GetComponent<CircleCollider2D>() ??
+                       throw new Exception($"Unable to assign colider2d in {name}");
+            var go = Instantiate(particleSystemObject, animator.gameObject.transform);
+            go.transform.localPosition = Vector3.zero;
+            
+            particleSystem = go.GetComponent<ParticleSystem>() ??
+                             throw new Exception($"Unable to assign particle system in {name}");
+        }
+
+        public override void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+            if (ShouldEnterClosing()) animator.SetTrigger(Closing);
+
+            HandleColliderLerp();
+            HandleParticleSystemLerp();
+        }
+
+        private void HandleParticleSystemLerp() {
+            var shape = particleSystem.shape;
+            shape.radius = collider.radius;
+        }
+
+        private void HandleColliderLerp() {
+            lerpTimeLeft = Mathf.Clamp(lerpTimeLeft - Time.deltaTime, 0, lerpDuration);
+            collider.radius = Mathf.Lerp(collider.radius, range, lerpTimeLeft);
+        }
+
+        public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+            animator.ResetTrigger(Closing);
+        }
+
+        private bool ShouldEnterClosing() {
+            stateTimeLeft = Mathf.Clamp(stateTimeLeft - Time.deltaTime, 0, activeDuration);
+            return stateTimeLeft <= 0;
+            ;
+        }
+    }
+}
---
+++ Assets/Scripts/Safe Zones/SafeZoneClosingState.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using UnityEngine;
+
+namespace Safe_Zones {
+    public class SafeZoneClosingState : StateMachineBehaviour {
+        private float timeLeft;
+        [SerializeField] private float activeDuration;
+        private static readonly int Closed = Animator.StringToHash("Closed");
+        private CircleCollider2D collider;
+        private ParticleSystem particleSystem;
+
+        public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+            timeLeft = activeDuration;
+            collider = animator.gameObject.GetComponent<CircleCollider2D>() ??
+                       throw new Exception($"Unable to assign colider2d in {name}");
+            particleSystem = animator.gameObject.GetComponentInChildren<ParticleSystem>() ??
+                 throw new Exception($"Unable to assign particle system in {name}");
+        }
+
+        public override void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+            if (ShouldEnterClosed()) animator.SetTrigger(Closed);
+            collider.radius = Mathf.Lerp(collider.radius, 0, timeLeft);
+            SetParticleSystemRadius();
+        }
+
+        private void SetParticleSystemRadius() {
+            var shape = particleSystem.shape;
+            shape.radius = collider.radius;
+        }
+
+        public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+            animator.ResetTrigger(Closed);
+            particleSystem.Stop();
+            particleSystem.Clear();
+            Destroy(particleSystem.gameObject);
+        }
+
+        private bool ShouldEnterClosed() {
+            timeLeft = Mathf.Clamp(timeLeft - Time.deltaTime, 0, activeDuration);
+            return timeLeft <= 0;
+            ;
+        }
+    }
+}
