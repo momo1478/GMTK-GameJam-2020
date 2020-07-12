bdb458e70e1396e0612399de33d22ae5a66736c7
momo1478 <monish1478@gmail.com>
2020-07-11 22:24:12 -0700

    Camera shake

--- Assets/Scripts/Player/PlayerCollides.cs
+++ Assets/Scripts/Player/PlayerCollides.cs
@@ -1,15 +1,23 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using Cinemachine;
+using DG.Tweening;
 using UnityEngine;
 
 public class PlayerCollides : MonoBehaviour
 {
+    CinemachineVirtualCamera cinemachineVirtualCamera;
+    private void Start()
+    {
+        cinemachineVirtualCamera = GameObject.Find("CM vcam1").GetComponent<CinemachineVirtualCamera>();   
+    }
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if(collision.gameObject.GetComponent<Projectile>() != null)
         {
             Destroy(collision.gameObject);
             GameManager.instance.Damage(1);
+            var noise = cinemachineVirtualCamera.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>();
+            noise.m_AmplitudeGain = 5f;
+            DOTween.To(() => noise.m_AmplitudeGain, (x) => noise.m_AmplitudeGain = x, 0, 0.5f);
         }
     }
 }
